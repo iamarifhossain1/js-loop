4e783027fd54c92d65b4c3330ec0b1465251337d
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-07-30 04:00:32 +0600

    while-loop.js

---
+++ while-loop.js
@@ -0,0 +1,17 @@
+const number = 7;
+
+if (number < 10) {
+    console.log ('Choto Number');
+}
+
+console.log ('Something new is cooking :D ');
+
+// -------------While-Loop-----------------------
+
+let num = 0; // Loop Variable
+while (num < 10) {
+    console.log ('Loooooooooooping');
+    num = num + 1; // num += or num++
+    
+
+}
