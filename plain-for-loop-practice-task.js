f5e674bca26b9b085b2879e45dba7d45c372eb07
iamarifhossain1 <sporshoahmed2667@gmail.com>
2025-07-30 14:35:04 +0600

    js-loop

---
+++ plain-for-loop-practice-task.js
@@ -0,0 +1,44 @@
+/***
+
+"I will invest at least 6 hrs every single day for next 60 days!" this message 60 times. So display this.
+
+ */
+
+
+// for (let i = 0; i <= 60; i++) {
+//     console.log ('Count:', i, 'I will invest at least 6 hrs every single day for next 60 days!');
+// }
+
+
+/***
+
+Subtask-1:
+
+Find all the odd numbers from 61 to 100.
+
+ */
+
+
+/***
+
+Subtask-2:
+
+Find all the even numbers from 78 to 98.
+
+ */
+
+// Odd Number Finder
+
+// for (let i = 61; i <= 100; i++) {
+//     if (i % 2 === 1) {
+//         console.log ('Odd Number:', i);
+//     }
+// }
+
+// Even Number Finder
+
+for (let i = 68; i <= 78; i++) {
+    if (i % 2 === 0) {
+        console.log ('Even Number:', i);
+    }
+}
